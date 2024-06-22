f658537419f762ed6d6e4edb25fd15433cb0e88b
jumperblumenau <jumperblumenau2018@gmail.com>
2024-06-22 10:12:42 -0300

    bruno j

--- MercadoPy/mercado.py
+++ MercadoPy/mercado.py
@@ -54,7 +54,7 @@ def cadastrar_produto() -> None:
     print('===================')
 
     nome: str = input('Informe o nome do produto: ')
-    preco: float = int(input('Informe o preço do produto: '))
+    preco: float = float(input('Informe o preço do produto: '))
 
     produto: Produto = Produto(nome, preco)
 
